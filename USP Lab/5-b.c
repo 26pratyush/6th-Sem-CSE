5668bf1fca2eeabb2a3bf1b4e3ccc5b69f8ac91f
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-04-14 11:19:07 +0530

    Create 5-b.c

---
+++ USP Lab/5-b.c
@@ -0,0 +1,21 @@
+//5.b) Write a program to differentiate between dup and dup2 functions
+
+#include <stdio.h>
+#include <unistd.h>                                                                                                                                         #include <fcntl.h>                                                                                                                                                                                                                                                                                                      int main() {                                                                                                                                                    
+
+int file = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);   
+                                                                                                                                                                                                                                                       // Using dup() - Duplicates the file descriptor (gets the smallest available file)
+    int dup_file = dup(file);
+
+    write(dup_file, "Written using dup()\n", 20); 
+    close(dup_file);
+
+    int dup2_file = dup2(file, 3);  // Using dup2() - Duplicates the file desc and explicitly specifies the target (file 3)
+
+    write(dup2_file, "Written using dup2()\n", 20);  
+    close(dup2_file);
+
+    close(file);
+
+    return 0;
+}           
