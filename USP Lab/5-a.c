c1f4e8a340cf3744801ed2534137f3367b4ecbd8
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-04-15 19:11:34 +0530

    added user input in 5a.c

--- USP Lab/5-a.c
+++ USP Lab/5-a.c
@@ -3,19 +3,24 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-int main(int argc, char *argv[])       //Send filename as argument:- ./a.out test.txt
-{
+int main() {
+    
+    char filename[20];
     struct stat st;
-    lstat(argv[1], &st);		//lstat
 
-    if (S_ISREG(st.st_mode)) printf("Regular file\n");
+    printf("Enter the filename: ");
+    scanf("%s", filename);  // Read filename from user input
+
+    lstat(filename, &st);
+
+    if (S_ISREG(st.st_mode))      printf("Regular file\n");
     else if (S_ISDIR(st.st_mode)) printf("Directory\n");
     else if (S_ISLNK(st.st_mode)) printf("Symbolic link\n");
     else if (S_ISCHR(st.st_mode)) printf("Character device\n");
     else if (S_ISBLK(st.st_mode)) printf("Block device\n");
-    else if (S_ISFIFO(st.st_mode)) printf("FIFO (named pipe)\n");
-    else if (S_ISSOCK(st.st_mode)) printf("Socket\n");
-    else printf("Unknown file type\n");
+    else if (S_ISFIFO(st.st_mode))printf("FIFO (named pipe)\n");
+    else if (S_ISSOCK(st.st_mode))printf("Socket\n");
+    else                          printf("Unknown file type\n");
 
     return 0;
 }
