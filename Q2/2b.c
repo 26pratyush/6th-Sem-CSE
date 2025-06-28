c6a505c3b23ec784159e15cb093643776cee3ac7
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-06-28 11:06:33 +0530

    Update and rename 2-b.c to 2b.c

---
+++ Q2/2b.c
@@ -0,0 +1,39 @@
+//2.b) Write a C program to simulate system function. 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+
+int my_sys(const char *cmd)
+{
+        if(!cmd){
+                return -1;
+        }
+
+        pid_t childpid=fork();
+
+        if(childpid==0){
+
+                execl("/bin/sh", "sh", "-c",cmd, (char *)NULL);
+                perror("execl failed");
+                exit(1);
+        }
+
+        int exit_status;
+
+        if(childpid>0 && waitpid(childpid, &exit_status, 0)!=-1 && WIFEXITED(exit_status))
+        {
+                return WEXITSTATUS(exit_status);
+        }
+        return -1;
+}
+
+int main(){
+        const char *cmd="ls -li";
+        int res=my_sys(cmd);
+        printf("Exit Status: %d\n", res);
+        return 0;
+}
+
+//Removed all error handling and messages
+
