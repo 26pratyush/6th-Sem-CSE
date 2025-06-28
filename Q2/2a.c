37e477e802cc4ba853a73c5a4c6e0026e3d3d14d
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-06-28 10:26:30 +0530

    Update and rename 2-a.c to 2a.c

--- Q2/2a.c
+++ Q2/2a.c
@@ -5,17 +5,17 @@
 
 int main() {
     char filename[100];
-    struct stat fileStat;
+    struct stat fileStat;                                            //declare
 
     printf("Enter the file name: ");
     scanf("%s", filename);
 
     stat(filename, &fileStat);
 
-    printf("File Size      : %ld bytes\n", fileStat.st_size);
+    printf("File Size      : %ld bytes\n", fileStat.st_size);        //fileStat.st_xyz
     printf("Number of Links: %ld\n", fileStat.st_nlink);
-    printf("Inode Number   : %ld\n", fileStat.st_ino);
-    printf("File Mode      : %o\n", fileStat.st_mode);
+    printf("Inode Number   : %ld\n", fileStat.st_ino);               
+    printf("File Mode      : %o\n", fileStat.st_mode);               // %o not %ld
     printf("Last Access    : %ld\n", fileStat.st_atime);
 
     return 0;
