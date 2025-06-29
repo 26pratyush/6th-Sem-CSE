8c33434bdcff7f1dc557adcaed76600add4990f5
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-06-29 16:29:07 +0530

    Update and rename 10-b.c to 10b.c

--- Q10/10b.c
+++ Q10/10b.c
@@ -10,7 +10,7 @@ int main() {
     if (c1 == 0) {
         printf("Child 1 (PID %d) running...\n", getpid());
         sleep(1);
-        return 1;  // Child 1 exits
+        return 1;  // Exit with status 1
     }
 
     pid_t c2 = fork();
@@ -18,16 +18,18 @@ int main() {
     if (c2 == 0) {
         printf("Child 2 (PID %d) running...\n", getpid());
         sleep(2);
-        return 2;  // Child 2 exits
+        return 2;  // Exit with status 2
     }
 
     int status;
 
-    wait(&status);  					// Wait for any child
-    printf("wait: Child exited with status %d\n", WEXITSTATUS(status));
+    // Wait for any one child to finish
+    wait(&status);
+    printf("wait: Child with PID %d exited with status %d\n",c1, WEXITSTATUS(status));
 
-    waitpid(c2, &status, 0);  				// Wait specifically for child 2
-    printf("waitpid: Child 2 exited with status %d\n", WEXITSTATUS(status));
+    // Wait specifically for c2
+    waitpid(c2, &status, 0);
+    printf("waitpid: Child 2 PID %d exited with status %d\n",c2, WEXITSTATUS(status));
 
     return 0;
 }
