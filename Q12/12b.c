9cf4448f5cf76152e9876d77d4c38dd13168b143
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-06-30 09:51:56 +0530

    Update 12b.c

--- Q12/12b.c
+++ Q12/12b.c
@@ -4,43 +4,42 @@
 //iii) The Parent should execute a process that prints default values for the environment variables.
 
 //main.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
-int main(void) {
-    pid_t pid;
-    char *env[] = { "USER=custom_user", "PATH=/custom", NULL };
+int main()
+{
+        char *env[] = {"USER=customer_user", "PATH=/custom", (char *)NULL};          // Custom environment variables for child
 
-    pid = fork();
+        pid_t pid = fork();                                                 
 
-    if (pid == 0) {
-        // Child: run echoall with custom environment
-        execle("./echoall", "echoall", "arg1", "arg2", NULL, env);
-        exit(1);  // if execle fails
-    } else {
-        wait(NULL);  // Wait for child
-        // Parent: run echoall with inherited environment
-        execlp("./echoall", "echoall", "default_arg", NULL);
-        exit(1);  // if execlp fails
-    }
+        if (pid == 0)                              
+                execle("./echoall", "echoall", "arg1", "arg2", (char *)NULL, env);   // Execute echoall with custom env (ensure 2nd prog name is echoall.c)
+                
+        else {
+                wait(NULL);                         
+                execlp("./echoall", "echoall", "default_arg", (char *)NULL);         // Execute echoall with inherited env (if changing prog name, change this)
+        }
 
-    return 0;
+        return 0;                                  
 }
+
 
 //echoall.c
-#include <stdio.h>
+#include <stdio.h> 
 
-int main(int argc, char *argv[], char *envp[]) {
-    for (int i = 0; i < argc; i++)
-        printf("argv[%d] = %s\n", i, argv[i]);
+int main(int argc, char *argv[], char *env[])            // command-line and environment arguments
+{
+        for(int i = 0; i < argc; i++)                    
+                printf("argv[%d]= %s\n", i, argv[i]);    // Print each argv[i]
 
-    for (int i = 0; envp[i] != NULL; i++)
-        printf("envp[%d] = %s\n", i, envp[i]);
+        for(int i = 0; env[i] != NULL; i++)              
+                printf("env[%d]= %s\n", i, env[i]);      // Print each environment variable
 
-    return 0;
+        return 0;                                        
 }
+
 
 //To run:
 gcc -o main main.c
